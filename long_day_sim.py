9520c75563ac6ca58dd925bea6d8708efe1ce7c6
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2025-06-18 13:20:40 -0600

    Auto-format with ruff, add pyright checking

--- long_day_sim.py
+++ long_day_sim.py
@@ -3,19 +3,21 @@ import dataclasses
 from typing import Literal, Union
 from pathlib import Path
 
-from ics import Calendar, Event
+from ics import Calendar, Event  # pyright: ignore[reportMissingTypeStubs]
+
 
 @dataclasses.dataclass
 class SimConfig:
     bedtime_on_start_day: time
-    start_day: date # "today"
+    start_day: date  # "today"
     sleep_duration: timedelta
-    day_length: timedelta # like "25 hours"
-    stop_date_time: datetime # end the simulation here
+    day_length: timedelta  # like "25 hours"
+    stop_date_time: datetime  # end the simulation here
 
     @property
     def awake_duration(self) -> timedelta:
         return self.day_length - self.sleep_duration
+
 
 @dataclasses.dataclass
 class Period:
@@ -26,13 +28,16 @@ class Period:
     def duration(self) -> timedelta:
         return self.end - self.start
 
+
 default_sim_config = SimConfig(
-    bedtime_on_start_day = time(1, 30),
-    start_day = datetime.now().date(),
-    sleep_duration = timedelta(hours = 8),
-    day_length = timedelta(hours = 25),
-    stop_date_time = (datetime.now() + timedelta(days=30)),
+    bedtime_on_start_day=time(1, 30),
+    start_day=datetime.now().date(),
+    sleep_duration=timedelta(hours=8),
+    # Note: 28h allows making it to one weekly meeting at the same awake time each week.
+    day_length=timedelta(hours=25),
+    stop_date_time=(datetime.now() + timedelta(days=30)),
 )
+
 
 def convert_local_to_utc(dt: datetime) -> datetime:
     dtz_local = dt.astimezone()
@@ -40,14 +45,16 @@ def convert_local_to_utc(dt: datetime) -> datetime:
     # print(f"Converted {dtz_local} to {dtz_utc}")
     return dtz_utc.replace(tzinfo=None)
 
+
 def write_to_ics_files(
-    sleep_periods: list[Period], awake_periods: list[Period],
-    periods_to_include: set[Literal['awake', 'sleep']],
-    ics_file: Union[str, Path]
+    sleep_periods: list[Period],
+    awake_periods: list[Period],
+    periods_to_include: set[Literal["awake", "sleep"]],
+    ics_file: Union[str, Path],
 ):
     c = Calendar()
 
-    if 'sleep' in periods_to_include:
+    if "sleep" in periods_to_include:
         for period in sleep_periods:
             e = Event()
             e.name = "Sleep"
@@ -55,7 +62,7 @@ def write_to_ics_files(
             e.end = convert_local_to_utc(period.end)
             c.events.add(e)
 
-    if 'awake' in periods_to_include:
+    if "awake" in periods_to_include:
         for period in awake_periods:
             e = Event()
             e.name = "Awake"
@@ -63,18 +70,21 @@ def write_to_ics_files(
             e.end = convert_local_to_utc(period.end)
             c.events.add(e)
 
-    with open(ics_file, 'w') as fp:
+    with open(ics_file, "w") as fp:
         fp.writelines(c.serialize_iter())
+
 
 def run_sim_print_data(sim_config: SimConfig):
     sleep_periods: list[Period] = []
     awake_periods: list[Period] = []
 
     # cur_dt is always a sleep start time
-    cur_dt: datetime = datetime.combine(sim_config.start_day, sim_config.bedtime_on_start_day)
+    cur_dt: datetime = datetime.combine(
+        sim_config.start_day, sim_config.bedtime_on_start_day
+    )
     print(f"Go to sleep at {cur_dt}")
 
-    last_awake_start: datetime = cur_dt - sim_config.awake_duration
+    _last_awake_start: datetime = cur_dt - sim_config.awake_duration
     last_sleep_start: datetime = cur_dt
 
     while cur_dt <= sim_config.stop_date_time:
@@ -87,16 +97,19 @@ def run_sim_print_data(sim_config: SimConfig):
         sleep_periods.append(Period(last_sleep_start, next_awake_start))
         awake_periods.append(Period(next_awake_start, next_sleep_start))
 
-        last_awake_start = next_awake_start
+        _last_awake_start = next_awake_start
         last_sleep_start = next_sleep_start
         cur_dt = next_sleep_start
 
     print(f"Sleep periods: {sleep_periods}")
     print(f"Awake periods: {awake_periods}")
 
-    write_to_ics_files(sleep_periods, awake_periods, {'sleep'}, '25h_day_schedule.ics')
+    write_to_ics_files(sleep_periods, awake_periods, {"sleep"}, "long_day_schedule.ics")
 
-    print(f"Wrote {len(sleep_periods)} sleep periods and {len(awake_periods)} awake periods to file.")
+    print(
+        f"Wrote {len(sleep_periods)} sleep periods and {len(awake_periods)} awake periods to file."
+    )
+
 
 if __name__ == "__main__":
     run_sim_print_data(default_sim_config)
